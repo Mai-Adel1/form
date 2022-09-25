244c08594a33164cefb20bf49f0c0c9b9d5e7c05
Mai-Adel1 <66090457+Mai-Adel1@users.noreply.github.com>
2022-09-25 07:41:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function myFunction(){
+    var myCon = document.getElementById('con').value;
+    //you should write letters 
+    let result = /[A-Za-z]/g;
+    if(result.test(myCon)){
+        document.getElementById('mess1').style.color='blue';
+        document.getElementById('mess1').innerHTML='valid';
+    }else{
+        document.getElementById('mess1').style.color='red';
+        document.getElementById('mess1').innerHTML='not valid';
+    }
+
+}
+
+function tell(){
+    da=document.getElementById('nam').value
+    document.getElementById('hi').innerHTML=`Thank You ${da} .`;
+    po = document.getElementById('pro').value;
+    on = document.getElementById('from').value;
+    to = document.getElementById('to').value;
+    document.getElementById('hi').innerHTML=`Thank You ${da} , We will transport your ${po} from ${on} to ${to}.`;
+}
+
+function phonevalidation(){
+    let phone = document.getElementById('phone').value;
+    let regex = /[0-9]{3}-[0-9]{3}-[0-9]{3}-[0-9]{2}/;
+    if(regex.test(phone)){
+        document.getElementById('vali').innerHTML='✓✓';
+    }else{
+        document.getElementById('vali').innerHTML='please enter the phe number in the correct way';
+    }
+    
+}
